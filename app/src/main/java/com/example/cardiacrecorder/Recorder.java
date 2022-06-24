da9a9c1550576faab847577d702fd5fa7b27a80f
TUSHI26 <tushi1807026@stud.kuet.ac.bd>
2022-06-24 11:32:53 +0600

    all files are added

--- app/src/main/java/com/example/cardiacrecorder/Recorder.java
+++ app/src/main/java/com/example/cardiacrecorder/Recorder.java
@@ -1,28 +1,33 @@
 package com.example.cardiacrecorder;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class Recorder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button2);
+        Button button = findViewById(R.id.button);//New_measure
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,reigistration.class);
+                Intent intent=new Intent(Recorder.this,New_measurement.class);
                 startActivity(intent);
                 finish();
-
             }
         });
     }
+
 }
